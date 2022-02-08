8d7a50d25718d88d6cb4007ed0bea0085ed8fb8f
naomichi-h <naomichi.programming@gmail.com>
2022-02-08 17:48:36 +0900

    記録の詳細機能作成

--- app/models/log.rb
+++ app/models/log.rb
@@ -2,4 +2,41 @@ class Log < ApplicationRecord
   def start_time
     self.date
   end
+
+  def medicine?
+    if self.medicine
+      '飲めた'
+    else
+      '飲めなかった'
+    end
+  end
+
+  def meal?
+    if self.meal
+      '摂れた'
+    else
+      '摂れなかった'
+    end
+  end
+
+  def bathe?
+    if self.bathe == :voluntary
+      '自発的に入った'
+    elsif self.bathe == :prompted
+      '促されて入った'
+    else
+      '入らなかった'
+    end
+  end
+
+  def go_out?
+    if self.go_out == :alone
+      '一人で外出した'
+    elsif self.go_out == :with_someone
+      '誰かと外出した'
+    else
+      '外出しなかった'
+    end
+  end
+
 end
