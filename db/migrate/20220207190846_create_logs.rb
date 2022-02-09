c43882fc56a973a29ec9cb0960c4a684c66df649
naomichi-h <naomichi.programming@gmail.com>
2022-02-09 15:46:44 +0900

    logテーブルに制限追加

--- db/migrate/20220207190846_create_logs.rb
+++ db/migrate/20220207190846_create_logs.rb
@@ -1,16 +1,18 @@
 class CreateLogs < ActiveRecord::Migration[6.1]
   def change
     create_table :logs do |t|
-      t.bigint :user_id
-      t.date :date
-      t.integer :sleep
-      t.boolean :meal
-      t.boolean :medicine
-      t.string :bathe
-      t.string :go_out
+      t.bigint :user_id, null: false
+      t.date :date, null: false
+      t.integer :sleep, null: false
+      t.boolean :meal, default: false, null: false
+      t.boolean :medicine, default: false, null: false
+      t.string :bathe, null: false
+      t.string :go_out, null: false
       t.text :memo
 
       t.timestamps
     end
+
+    add_index :logs, :date, unique: true
   end
 end
