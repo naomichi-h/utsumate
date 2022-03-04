4a454409cbbccb0c3b6b91525763e6ede18029ba
naomichi-h <naomichi.programming@gmail.com>
2022-03-04 16:05:43 +0900

    受け取ったデータを集計して出力するロジックを組んだ

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,6 +4,19 @@ class ReportsController < ApplicationController
 
   def aggregate
     @logs = Log.where('date >= ?', params[:start_date])
+    @sleeps = @logs.map(&:sleep)
+    @sleeps_average = (@sleeps.sum.to_f/@sleeps.length).round(1)
+    @meals = @logs.map(&:meal)
+    @meals_ratio = ((@meals.count(true).to_f/@meals.length)*100).round(1)
+    @medicines = @logs.map(&:medicine)
+    @medicines_ratio = ((@medicines.count(true).to_f/@medicines.length)*100).round(1)
+    @bathes = @logs.map(&:bathe)
+    @bathes_voluntary_ratio = ((@bathes.count("voluntary").to_f/@bathes.length)*100).round(1)
+    @bathes_prompted_ratio = ((@bathes.count("prompted").to_f/@bathes.length)*100).round(1)
+    @go_outs = @logs.map(&:go_out)
+    @go_outs_alone_ratio = ((@go_outs.count("alone").to_f/@bathes.length)*100).round(1)
+    @go_outs_with_someone_ratio = ((@go_outs.count("with_someone").to_f/@go_outs.length)*100).round(1)
+    @memos = @logs.map(&:memo)
     render :index
   end
 
