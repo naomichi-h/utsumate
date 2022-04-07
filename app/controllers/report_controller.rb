fdbb537ff1716085ed6c595569fa0b95942730b2
naomichi-h <naomichi.programming@gmail.com>
2022-04-07 15:16:03 +0900

    うつレポ出力機能の細かい修正

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -4,31 +4,29 @@ class ReportController < ApplicationController
   end
 
   def index
-    @logs = Log.where('date >= ?', params[:report_start_date])
-    @sleeps = @logs.map(&:sleep)
+    @logs_from_report_start_date = Log.where('date >= ?', params[:report_start_date])
     if params[:start_date]
       chart_start_date = params[:start_date].to_date.beginning_of_month.to_s
-      chart_end_date = params[:start_date].to_date.end_of_month.to_s
+      @chart_end_date = params[:start_date].to_date.end_of_month.to_s
     else
       chart_start_date = Date.today.beginning_of_month.to_s
-      chart_end_date = Date.today.end_of_month.to_s
+      @chart_end_date = Date.today.end_of_month.to_s
     end
-    @sleeps_chart = @logs.where('date >= ?', chart_start_date).where('date <= ?', chart_end_date).map{|h| h.values_at(:date, :sleep) }
-    unless @sleeps_chart.find{|arr| arr[0] == chart_end_date.to_date}
-      @sleeps_chart.push([chart_end_date, 0])
-    end
-    @sleeps_average = (@sleeps.sum.to_f/@sleeps.length).round(1)
-    @meals = @logs.map(&:meal)
-    @meals_ratio = ((@meals.count(true).to_f/@meals.length)*100).round(1)
-    @medicines = @logs.map(&:medicine)
-    @medicines_ratio = ((@medicines.count(true).to_f/@medicines.length)*100).round(1)
-    @bathes = @logs.map(&:bathe)
-    @bathes_voluntary_ratio = ((@bathes.count("voluntary").to_f/@bathes.length)*100).round(1)
-    @bathes_prompted_ratio = ((@bathes.count("prompted").to_f/@bathes.length)*100).round(1)
-    @go_outs = @logs.map(&:go_out)
-    @go_outs_alone_ratio = ((@go_outs.count("alone").to_f/@bathes.length)*100).round(1)
-    @go_outs_with_someone_ratio = ((@go_outs.count("with_someone").to_f/@go_outs.length)*100).round(1)
-    @memos = @logs.map(&:memo)
+    @logs_for_1month = @logs_from_report_start_date.where('date >= ?', chart_start_date).where('date <= ?', @chart_end_date)
+    @sleeps_chart = @logs_for_1month.map{|h| h.values_at(:date, :sleep) }
+    sleeps = @logs_for_1month.map(&:sleep)
+    @sleeps_average = (sleeps.sum.to_f/sleeps.length).round(1)
+    meals = @logs_for_1month.map(&:meal)
+    @meals_ratio = ((meals.count(true).to_f/meals.length)*100).round(1)
+    medicines = @logs_for_1month.map(&:medicine)
+    @medicines_ratio = ((medicines.count(true).to_f/medicines.length)*100).round(1)
+    bathes = @logs_for_1month.map(&:bathe)
+    @bathes_voluntary_ratio = ((bathes.count("voluntary").to_f/bathes.length)*100).round(1)
+    @bathes_prompted_ratio = ((bathes.count("prompted").to_f/bathes.length)*100).round(1)
+    go_outs = @logs_for_1month.map(&:go_out)
+    @go_outs_alone_ratio = ((go_outs.count("alone").to_f/bathes.length)*100).round(1)
+    @go_outs_with_someone_ratio = ((go_outs.count("with_someone").to_f/go_outs.length)*100).round(1)
+    @memos = @logs_for_1month.map(&:memo)
     render :index
   end
 
