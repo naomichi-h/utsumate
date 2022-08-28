be65d972c033fdddf6784e14cb30b8ec17ca151f
naomichi-h <naomichi.programming@gmail.com>
2022-08-28 21:00:56 +0900

    印刷プレビュー時のデザイン修正

--- app/javascript/packs/print.js
+++ app/javascript/packs/print.js
@@ -1,30 +1,70 @@
-function printA4() {
-  // プリントしたいエリア以外に、非表示のcssが付与されたclassを追加
-  const body = document.querySelector('body')
-  const children = body.children
+// 印刷ダイアログを開く前の処理
+function addPrintClass() {
+  return new Promise(function (resolve) {
+    const $body = document.querySelector('body')
+    const $children = $body.children
+    const $printArea = document.getElementById('print-area')
+    const $printAreaChildren = $printArea.children
+    Array.from($children).forEach(function (child) {
+      if (child.id !== 'print-area') {
+        child.classList.add('print-off')
+      }
+    })
 
-  Array.from(children).forEach(function (child) {
-    if (child.id !== 'print-area') {
-      child.classList.add('print-off')
-    }
+    Array.from($printAreaChildren).forEach(function (child) {
+      if (child.classList.contains('report-list')) {
+        child.classList.add('print-1page')
+      } else {
+        child.classList.add('print-width')
+      }
+    })
+    console.log('add')
+    resolve()
   })
+}
 
-  window.print()
+function print() {
+  return new Promise(function (resolve) {
+    //addPrintClass()で画面の変化が終了するのを待つ。これをしないとチャートの描画がおかしくなる。
+    setTimeout(() => {
+      window.print()
+      console.log('print')
+      resolve()
+    }, 1000)
+  })
+}
 
-  // window.print()を実行した後、非表示用のclass「print-off」を削除
-  Array.from(children).forEach(function (child) {
-    if (child.classList.contains('print-off')) {
-      child.classList.remove('print-off')
-    }
+function removePrintClass() {
+  return new Promise(function (resolve) {
+    const $body = document.querySelector('body')
+    const $children = $body.children
+    const $printArea = document.getElementById('print-area')
+    const $printAreaChildren = $printArea.children
+
+    Array.from($children).forEach(function (child) {
+      if (child.classList.contains('print-off')) {
+        child.classList.remove('print-off')
+      }
+    })
+
+    Array.from($printAreaChildren).forEach(function (child) {
+      if (child.classList.contains('report-list')) {
+        child.classList.remove('print-1page')
+      } else {
+        child.classList.remove('print-width')
+      }
+    })
+    console.log('remove')
+    resolve()
   })
 }
 
 document.addEventListener('turbolinks:load', () => {
-  const button = document.getElementById('print-button')
-  if (!button) {
+  const $button = document.getElementById('print-button')
+  if (!$button) {
     return false
   }
-  button.addEventListener('click', () => {
-    printA4()
+  $button.addEventListener('click', () => {
+    addPrintClass().then(print).then(removePrintClass)
   })
 })
