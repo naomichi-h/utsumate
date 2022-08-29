4cc8be8b6adb1895793a4abe8b41acaace8342c1
naomichi-h <naomichi.programming@gmail.com>
2022-08-29 19:16:52 +0900

    未ログイン時のrootページのリダイレクト先をtopページに変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class HomeController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate
   def index; end
+end
+
+def authenticate
+  redirect_to top_path unless user_signed_in?
 end
