4a1b9e97451a4ef46665e49bb47fdf8bc4925b58
naomichi-h <naomichi.programming@gmail.com>
2022-08-30 10:34:49 +0900

    メソッドをprivate化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
     top_path
   end
 
+  private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
