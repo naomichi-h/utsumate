f95bea1d02e399953a2e1df51e13ce2653527755
naomichi-h <naomichi.programming@gmail.com>
2022-09-01 19:25:41 +0900

    routes.rbの不要な記述を削除

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   }
   root to: 'home#index'
   resources :logs
-  resources :users, only: %i(update), controller: "users"
+  resources :users, only: %i(update)
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
